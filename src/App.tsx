c1e800e0158112e3ea34988a0de3bca0b429a657
Jakub Molicki <molickikuba@gmail.com>
2022-03-17 12:33:16 +0100

    react bootstrap and radio-browser added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Container, Row, Col, CardGroup, Card, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 const App = () => {
-  return <div className="App">Hello</div>;
+  return (
+    <Container>
+      <Card>
+        <Card.Title>Elo</Card.Title>
+        <Card.Text>Lorem10</Card.Text>
+        <Button>Siema</Button>
+      </Card>
+    </Container>
+  );
 };
 
 export default App;
